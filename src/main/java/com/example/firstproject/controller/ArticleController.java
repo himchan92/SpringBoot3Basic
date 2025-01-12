b724bb38ffd0e9773c9fb29a28ad0cb3526fc532
omea92 <thdjun92@naver.com>
2025-01-13 00:23:35 +0900

    롬복 적용 DTO 간소화, slf4j 적용

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
@@ -20,13 +22,13 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
         //1.DTO -> Entity
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
         //2. repository -> entity save
         Article saved = articleRepository.save(article); //저장 JPA
-        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 }
